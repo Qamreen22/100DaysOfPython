33d5c95b5b24005cb1f4a4c5fc828f17bebe945d
Qamreen <117499080+Qamreen22@users.noreply.github.com>
2023-06-01 01:00:57 +0530

    Update 6. Reeborg's Maze.py

--- 6. Reeborg's Maze.py
+++ 6. Reeborg's Maze.py
@@ -5,6 +5,9 @@ Created on Thu Jun  1 00:54:06 2023
 @author: Dell
 """
 
+#The code is the solustion to Reeborg's Maze
+#Run the code at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
 def turn_right():
     turn_left()
     turn_left()
@@ -22,8 +25,7 @@ while not at_goal():
     else:
         turn_left()
         
-    
-
+   
 
 
 
